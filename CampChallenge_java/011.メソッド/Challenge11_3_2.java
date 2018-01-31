6b05817628fb6b6051529ee575f6429b262213dd
FurukawaTakuma <hkmn1717@gmail.com>
2018-01-31 12:58:41 +0900

    メソッド課題3_2_提出

---
+++ CampChallenge_java/011.メソッド/Challenge11_3_2.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+
+/**
+ *
+ * @author guest1Day
+ */
+
+public class Challenge11_3_2 extends HttpServlet {
+    
+        HashMap<String, String> Hairetsu(String Hairetsu){
+            HashMap<String,String> Data = new HashMap<String,String>();
+            Data.put("ID","ID.No");
+            Data.put("Name","名前");
+            Data.put("BirthDay","生年月日");
+            Data.put("Address","住所");
+            return Data;
+        }
+
+    
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+
+    
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Challenge11_3_2</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Challenge11_3_2 at " + request.getContextPath() + "</h1>");
+            
+            HashMap<String,String> Data = new HashMap<String,String>();
+            Data = Hairetsu("");
+            
+
+            out.println(Data.get("Name"));
+            out.println(Data.get("BirthDay"));
+            out.println(Data.get("Address"));
+            //out.println("<br>"+Data);            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
